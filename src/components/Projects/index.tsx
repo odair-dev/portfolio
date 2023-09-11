4d1fb2b7147c38d13c44ddf36237a8505aa92f6d
Odair <odairodriguez@yahoo.com.br>
2023-09-11 12:44:13 -0300

    feat: links projects

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -33,6 +33,10 @@ export default function Projects(){
             <div className={styles.subContainer}>
                 <div className={styles.gallery} data-aos="fade-right" data-aos-duration="1500">
                     <Image src={Recipe} onClick={()=>goRecipe()}  alt="Site de Receitas"  className={slide==1 ? styles.item_1 : slide==2 ? styles.item_2 : slide==3 ? styles.item_3 : styles.none}/>
+                    <div className={styles.divLinks}>
+                        <a target='_blank' href={slide==1 ? "https://github.com/odair-dev/burguer" : slide==2 ? "https://github.com/odair-dev/recipes" : slide==3 ? "https://github.com/odair-dev/nukenzie" : styles.none}>Github</a>
+                        <a target='_blank' href={slide==1 ? "https://burguer-five.vercel.app/" : slide==2 ? "https://recipes-silk-eight.vercel.app/" : slide==3 ? "https://react-entrega-s1-template-nu-kenzie-odair-dev.vercel.app/" : styles.none}>Deploy</a>
+                    </div>
                     <Image src={Burguer} onClick={()=>goBurguer()} alt="Site Hamburgueria" className={slide==1 ? styles.item_2 : slide==2 ? styles.item_3 : slide==3 ? styles.item_1 : styles.none}/>
                     <Image src={Nukenzie} onClick={()=>goNukenzie()} alt="Site Nukenzie"     className={slide==1 ? styles.item_3 : slide==2 ? styles.item_1 : slide==3 ? styles.item_2 : styles.none}/>
                     
